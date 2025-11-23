5a7210709d51a3d4676a89946d7ee180e8e80a6a
N11cc00 <nifrabe@outlook.de>
2025-11-23 21:06:03 +0100

    nfc-v minor change

--- sys/include/net/nfc/nfc_v.h
+++ sys/include/net/nfc/nfc_v.h
@@ -1,4 +1,13 @@
 #pragma once
 
+#define NFC_V_UID_LEN  (8u)
+
 typedef struct {
+    uint8_t res_flag;
+    uint8_t dsfid;
+    uint8_t uid[NFC_V_UID_LEN];
+} nfc_v_inventory_res_t;
+
+typedef struct {
+    nfc_v_inventory_res_t inventory_res;
 } nfc_v_listener_config_t;
